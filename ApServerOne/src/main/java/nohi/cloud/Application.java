9ae83054aaa348691d6c80000193dbdc694249b9
NOHI <thisisnohi@163.com>
2022-06-10 13:58:19 +0800

    feat 增加config 读取刷新

--- ApServerOne/src/main/java/nohi/cloud/Application.java
+++ ApServerOne/src/main/java/nohi/cloud/Application.java
@@ -1,8 +1,10 @@
 package nohi.cloud;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.ConfigurableApplicationContext;
 
 /**
  * @author NOHI
@@ -10,8 +12,12 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  **/
 @SpringBootApplication
 @EnableDiscoveryClient
+@Slf4j
 public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(Application.class, args);
+        String userName = applicationContext.getEnvironment().getProperty("user.name");
+        String userAge = applicationContext.getEnvironment().getProperty("user.age");
+        log.info("userName:{},userAge:{}", userName, userAge);
     }
 }
--- ApServerOne/src/main/java/nohi/cloud/aptwo/web/HelloController.java
+++ ApServerOne/src/main/java/nohi/cloud/aptwo/web/HelloController.java
@@ -1,6 +1,10 @@
 package nohi.cloud.aptwo.web;
 
 import lombok.extern.slf4j.Slf4j;
+import nohi.cloud.aptwo.config.ApConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +15,22 @@ import org.springframework.web.bind.annotation.RestController;
  **/
 @RestController
 @Slf4j
+@RefreshScope
 public class HelloController {
+    @Value("${user.name}")
+    String userName;
 
+    @Value("${user.age}")
+    int age;
 
+    @Autowired
+    private ApConfig apConfig;
     @GetMapping(value = "/")
     public String index() {
         log.info("HelloController.index...");
-        return "Hello";
+        log.info("apConfig:{}", null == apConfig ? "is null" : apConfig.toString());
+        log.info("userName:{} age:{}", userName, userName);
+        return "Hello " + userName + ", age:" + age + "\n" + apConfig.toString();
     }
 
     @GetMapping(value = "/echo/{string}")
